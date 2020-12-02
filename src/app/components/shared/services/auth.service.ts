1445a161ddc5e4271aa0e9f85b8afa029c657764
tbone7243 <tbone7243@gmail.com>
2020-12-02 13:07:04 -0500

    Finished staging and updating the last push request.

---
+++ src/app/components/shared/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { AuthData } from '../../shared/models/auth-data.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+
+  constructor(private http: HttpClient) { }
+
+  createUser(email: string, password: string) {
+    const authData: AuthData = {
+      email: email,
+      password: password
+    }
+    this.http.post('http://localhost:3000/api/users/signup', authData)
+    .subscribe(response => {
+      console.log(response);
+    });
+  }
+login(email: string, password: string) {
+  const authData: AuthData = {
+    email: email,
+    password: password
+  }
+  this.http.post('http://localhost:3000/api/users/login', authData)
+  .subscribe(response => {
+    console.log(response);
+  })
+}
+}
